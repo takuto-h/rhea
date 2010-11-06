893360af5390e52b74ee84b92daa66e0078641c2
Takuto Hayashi <takuto.hayashi@gmail.com>
2010-11-06 09:22:21 +0900

    Add EnvDefault

---
+++ src/Rhea/EnvDefault.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections.Generic;
+
+namespace Rhea
+{
+    public class EnvDefault : EnvGlobal
+    {
+        public EnvDefault()
+        {
+            this.AddVariable("p", 1, (args, vm, info) => {
+                Console.WriteLine(args[0]);
+                vm.Push(args[0]);
+            });
+            this.AddVariable("dynamic_wind", 3, (args, vm, info) => {
+                ValueCont cont = vm.GetCont();
+                var winder = new KeyValuePair<IValue, IValue>(args[0], args[2]);
+                var winders = SList.Cons(winder, vm.Winders);
+                Stack<IInsn> insnStack = new Stack<IInsn>();
+                insnStack.Push(new InsnCall(0, info));
+                insnStack.Push(InsnPop.Instance);
+                insnStack.Push(new InsnSetWinders(winders));
+                insnStack.Push(new InsnPush(args[1]));
+                insnStack.Push(new InsnCall(0, info));
+                insnStack.Push(new InsnCall(1, info));
+                vm.Insns = insnStack.ToSList();
+                vm.Stack = SList.List<IValue>(args[0], cont);
+            });
+            this.AddVariable("callcc", 1, (args, vm, info) => {
+                IValueFunc func = args[0] as IValueFunc;
+                if (func == null)
+                {
+                    throw new RheaException(
+                        string.Format("function required, but got {0}", args[0]),
+                        info
+                    );
+                }
+                ValueCont cont = vm.GetCont();
+                IList<IValue> newArgs = new List<IValue> { cont };
+                func.Call(newArgs, vm, info);
+            });
+        }
+    }
+}
--- src/Rhea/Interpreter.cs
+++ src/Rhea/Interpreter.cs
@@ -11,38 +11,7 @@ namespace Rhea
         
         public Interpreter()
         {
-            mEnv = new EnvGlobal();
-            mEnv.AddVariable("p", 1, (args, vm, info) => {
-                Console.WriteLine(args[0]);
-                vm.Push(args[0]);
-            });
-            mEnv.AddVariable("dynamic_wind", 3, (args, vm, info) => {
-                ValueCont cont = vm.GetCont();
-                var winder = new KeyValuePair<IValue, IValue>(args[0], args[2]);
-                var winders = SList.Cons(winder, vm.Winders);
-                Stack<IInsn> insnStack = new Stack<IInsn>();
-                insnStack.Push(new InsnCall(0, info));
-                insnStack.Push(InsnPop.Instance);
-                insnStack.Push(new InsnSetWinders(winders));
-                insnStack.Push(new InsnPush(args[1]));
-                insnStack.Push(new InsnCall(0, info));
-                insnStack.Push(new InsnCall(1, info));
-                vm.Insns = insnStack.ToSList();
-                vm.Stack = SList.List<IValue>(args[0], cont);
-            });
-            mEnv.AddVariable("callcc", 1, (args, vm, info) => {
-                IValueFunc func = args[0] as IValueFunc;
-                if (func == null)
-                {
-                    throw new RheaException(
-                        string.Format("function required, but got {0}", args[0]),
-                        info
-                    );
-                }
-                ValueCont cont = vm.GetCont();
-                IList<IValue> newArgs = new List<IValue> { cont };
-                func.Call(newArgs, vm, info);
-            });
+            mEnv = new EnvDefault();
         }
         
         public void InterpretInteractively()
